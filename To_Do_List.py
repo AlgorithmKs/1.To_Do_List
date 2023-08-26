c0a16364d8977688dd398a929b1ff9469ea9e7d4
AsusVida <mailboxer@mail.com>
2023-08-26 21:37:27 +0400

    Added an exception if the user enters an invalid value in the menu.

--- To_Do_List.py
+++ To_Do_List.py
@@ -10,25 +10,28 @@ def instruction():
 
 
 while user_input != '4':
-    instruction()
-    user_input = input('Enter the number: ')
+    try:
+        instruction()
+        user_input = input('Enter the number: ')
 
-    if user_input == '1':
-        task = input('What is to be done? ')
-        data.append(task)
-        print('Task added to the list: ', task)
-    elif user_input == '2':
-        task = input('What has been done? ')
-        if task in data:
-            data.remove(task)
-            print('Task', task, 'removed from the list')
+        if user_input == '1':
+            task = input('What is to be done? ')
+            data.append(task)
+            print('Task added to the list: ', task)
+        elif user_input == '2':
+            task = input('What has been done? ')
+            if task in data:
+                data.remove(task)
+                print('Task', task, 'removed from the list')
+            else:
+                print('List does not contain this task')
+        elif user_input == '3':
+            print('List of deals:')
+            for task in data:
+                print(task)
+        elif user_input == '4':
+            print('See you!')
         else:
-            print('List does not contain this task')
-    elif user_input == '3':
-        print('List of deals:')
-        for task in data:
-            print(task)
-    elif user_input == '4':
-        print('See you!')
-    else:
-        print('Please enter any number from 1 to 4')
+            raise ValueError
+    except ValueError:
+        print("Error: Please enter any number from 1 to 4")
